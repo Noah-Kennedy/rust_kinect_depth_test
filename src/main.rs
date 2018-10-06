f62cb0f5d2419a93c8abddaae096545467f9c3a5
noah <nomaxx117@gmail.com>
2018-10-06 16:01:17 -0500

    Cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,12 @@ mod glwinhelp;
 use glwinhelp::imgwin;
 
 use std::time::Duration;
-
+use std::io::Result;
 use freenectrs::freenect;
 
 #[inline]
 fn convert_raw_kinect_data_to_meters(raw_depth: f32) -> f32 {
-    //return 0.1236 * f32::tan((raw_depth / 2842.5) + 1.1863);
     return 1.0 / (raw_depth * -0.0030711016 + 3.3309495161);
-
 }
 
 #[inline]
@@ -27,21 +25,20 @@ fn spectrum_curve(input: f32) -> f32 {
 
 #[inline]
 fn convert_depth_to_rgb(distance: f32) -> (u8, u8, u8) {
-    
     if distance < 0.0 {
         return (0, 0, 0);
     }
-    
+
     let decimal: f32 = distance - ((distance as u8) as f32);
-    
+
     let spectrum: f32 = 6.0 * ((3.0 * decimal.powi(2)) - (2.0 * decimal.powi(3)));
-    
+
     let red = spectrum_curve(((spectrum + 2.0) % 6.0) - 2.0);
     let green = spectrum_curve(spectrum - 2.0);
     let blue = spectrum_curve(((spectrum + 4.0) % 6.0) - 2.0);
-    
+
     let result = (red as u8, blue as u8, green as u8);
-    
+
     return result;
 }
 
@@ -49,46 +46,51 @@ fn convert_depth_to_rgb(distance: f32) -> (u8, u8, u8) {
 fn depth_to_img(data: &[u16]) -> image::RgbaImage {
     image::RgbaImage::from_fn(640, 480, |x, y| {
         let idx = y * 640 + x;
-        
+
         let raw_data = data[idx as usize] as f32;
-        
+
         let depth = convert_raw_kinect_data_to_meters(raw_data);
-        
+
         if x == 640 / 2 && y == 480 / 2 {
             println!("{}", depth);
             return image::Rgba([255, 255, 255, 255]);
         }
-        
+
         let (red, blue, green) = convert_depth_to_rgb(depth);
-        
+
         image::Rgba([red, green, blue, 255])
     })
 }
 
-pub fn main() {
-    let ctx = freenect::FreenectContext::init_with_video_motor().unwrap();
-    let dev_count = ctx.num_devices().unwrap();
-    if dev_count == 0 {
-        println!("No device connected - abort");
-        return;
-    } else {
-        println!("Found {} devices, use first", dev_count);
+fn push_image_to_window(dstream: &freenect::FreenectDepthStream,
+                        mut dimg: image::RgbaImage,
+                        dwin: &mut imgwin::ImgWindow) -> image::RgbaImage {
+    let _ = imgwin::FixWaitTimer::new(Duration::from_millis(1000 / 25));
+    if let Ok((data, _ /* timestamp */)) = dstream.receiver.try_recv() {
+        dimg = depth_to_img(&*data);
     }
-    let device = ctx.open_device(0).unwrap();
+    dwin.set_img(dimg.clone());
+    dwin.redraw();
+
+    return dimg;
+}
+
+pub fn main() {
+    let freenect_context = freenect::FreenectContext::init_with_video_motor().unwrap();
+    let device_count = freenect_context.num_devices().unwrap();
+
+    let device = freenect_context.open_device(0).expect("No device connected - abort");
+
     device.set_depth_mode(freenect::FreenectResolution::Medium,
                           freenect::FreenectDepthFormat::Bit11).unwrap();
-    
-    let mut dwin = imgwin::ImgWindow::new("Live Depth");
 
-    let dstream = device.depth_stream().unwrap();
-    let mut dimg = image::RgbaImage::new(640, 480);
-    ctx.spawn_process_thread().unwrap();
+    let mut depth_window = imgwin::ImgWindow::new("Live Depth");
+
+    let depth_stream = device.depth_stream().unwrap();
+    let mut depth_image = image::RgbaImage::new(640, 480);
+    freenect_context.spawn_process_thread().unwrap();
+
     loop {
-        let _ = imgwin::FixWaitTimer::new(Duration::from_millis(1000 / 25));
-        if let Ok((data, _ /* timestamp */)) = dstream.receiver.try_recv() {
-            dimg = depth_to_img(&*data);
-        }
-        dwin.set_img(dimg.clone());
-        dwin.redraw();
+        depth_image = push_image_to_window(&depth_stream, depth_image, &mut depth_window);
     }
 }
